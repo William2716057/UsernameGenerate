25564a33f0348167c30ead6a5637fce54da0bc19
User <Willspri@gmail.com>
2024-11-14 11:30:39 +1300

    error fixed

--- userList.sh
+++ userList.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-names=("Fred" "Johnson" "Ian Davidson" "Linda Smith" "Jason Anderson")
+names=("Fred Johnson" "Ian Davidson" "Linda Smith" "Jason Anderson")
 
 generateList() {
 	local arr=("$@")
 	local len=${#arr[@]}
 
 	for ((i=0; i<$len; i++)); do
-#		for ((j=i+1; j<$len; j++)); do
-		local space_remove="${arr[i]// /}"
-		echo "${space_remove}@gmail.com"
+	    local space_remove="${arr[i]// /}"
+	    echo "${space_remove}@gmail.com"
 	done
 }
 
